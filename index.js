f2a17507a74dcb16dab9732580a54996605a1833
cptbrkfst <dyl@resonategroup.ca>
2017-11-03 12:43:06 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Add your doToElementsInArray() function here:
-
-
+var doToElementsInArray = (arr, cb) => arr.forEach(cb);
 // Add your changeCompletely() function here:
+var changeCompletely = (element, index, array) => {
+  array[index] =
+    Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+};
